e95152c179cc60d03e23a0b4780ebab759b456a7
Pilla <manojkumar.pilla@dxc.com>
2023-04-20 16:52:13 +0530

    update

---
+++ repos/switch_case/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Enter First Number");
+int num1 = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Enter Second Number");
+int num2 = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Enter The operator");
+char opert = Convert.ToChar(Console.ReadLine());
+ 
+
+switch (opert)
+{
+    case '+':
+        {
+            int result = num1 + num2;
+            Console.WriteLine("Result is = " +result);
+            break;
+        }
+    case '-':
+        {
+            int result = num1 - num2;
+            Console.WriteLine("Result is = " + result);
+            break;
+    }
+    case '*':
+        {
+        int result = num1 * num2;
+        Console.WriteLine("Result is = " + result);
+        break;
+    }
+    case '%':
+        {
+        int result = num1 % num2;
+        Console.WriteLine("Result is = " + result);
+        break;
+    }
+    case '/':
+        {
+            int result = num1 - num2;
+            Console.WriteLine("Result is = " + result);
+            break;
+        }
+
+}
