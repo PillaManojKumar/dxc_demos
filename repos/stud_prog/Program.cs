6126ffed1892e54136b28861d9a981a08229b084
Pilla <manojkumar.pilla@dxc.com>
2023-04-20 16:14:08 +0530

    update stud_prog project

--- repos/stud_prog/Program.cs
+++ repos/stud_prog/Program.cs
@@ -16,41 +16,34 @@ int overall = 300;
 tot = marks1 + marks2 + marks3;
 per = (tot / overall) * 100;
 
-if( per > 50)
+Console.WriteLine("Your total score is = " + tot);
+Console.WriteLine("Your per is =" + per);
+
+if ( per > 50)
 {
-    if(per > 90 && per < 100)
+    if(per >= 90 && per <= 100)
     {
-        Console.WriteLine("Your total score is = " + tot);
-        Console.WriteLine("Your per is =" + per);
         Console.WriteLine("Your grade is A");
         Console.WriteLine("Your result for the exam is pass");
     }
-    else if(per > 80 && per < 89)
+    else if(per >= 80 && per <= 89)
     {
-        Console.WriteLine("Your total score is = " + tot);
-        Console.WriteLine("Your per is =" + per);
         Console.WriteLine("Your grade is B");
         Console.WriteLine("Your result for the exam is pass");
     }
-    else if (per > 60 && per < 79)
+    else if (per >= 60 && per <= 79)
     {
-        Console.WriteLine("Your total score is = " + tot);
-        Console.WriteLine("Your per is =" + per);
         Console.WriteLine("Your grade is C");
         Console.WriteLine("Your result for the exam is pass");
     }
-    else if (per > 50 && per < 59)
+    else if (per >= 50 && per <= 59)
     {
-        Console.WriteLine("Your total score is = " + tot);
-        Console.WriteLine("Your per is =" + per);
         Console.WriteLine("Your grade is D");
         Console.WriteLine("Your result for the exam is pass");
     }
 }
 else
 { 
-    Console.WriteLine("Your total score is = " + tot);
-    Console.WriteLine("Your per is =" + per);
     Console.WriteLine("Your grade is E");
     Console.WriteLine("Your result for the exam is fail");
 }
