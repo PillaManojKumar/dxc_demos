e893aec523068511ff72a6048ff14d90f9a82f07
Pilla <manojkumar.pilla@dxc.com>
2023-04-26 17:47:38 +0530

    updated

---
+++ repos/myapp/Caluclator/Class1.cs
@@ -0,0 +1,26 @@
+﻿namespace Caluclator
+{
+    public class Calculator
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine( a + b);
+        }
+        public void Sub(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+        public void Mul(int a, int b)
+        {
+            Console.WriteLine(a* b);
+        }
+        public void Div(int a, int b)
+        {
+            Console.WriteLine(a / b);
+        }
+        public void Mod(int a, int b)
+        {
+            Console.WriteLine(a % b);
+        }
+    }
+}
---
+++ repos/myapp/Myapp/Program.cs
@@ -0,0 +1,64 @@
+﻿using Caluclator;
+using System.ComponentModel;
+
+namespace myapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+
+            Console.WriteLine("Enter First Number");
+            int num1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter Second Number");
+            int num2 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter The operator");
+            char opert = Convert.ToChar(Console.ReadLine());
+
+
+            switch (opert)
+            {
+                case '+':
+                    {
+                        int result = num1 + num2;
+                        Console.WriteLine("Result is = " + result);
+                        break;
+                    }
+                case '-':
+                    {
+                        int result = num1 - num2;
+                        Console.WriteLine("Result is = " + result);
+                        break;
+                    }
+                case '*':
+                    {
+                        int result = num1 * num2;
+                        Console.WriteLine("Result is = " + result);
+                        break;
+                    }
+                case '/':
+                    {
+                        int result = num1 % num2;
+                        Console.WriteLine("Result is = " + result);
+                        break;
+                    }
+                case '%':
+                    {
+                        int result = num1 - num2;
+                        Console.WriteLine("Result is = " + result);
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Wrong operator");
+                        break;
+                    }
+
+            }
+            Console.ReadKey();
+
+        }
+
+    }
+}
