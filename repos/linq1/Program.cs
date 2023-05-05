310124090595bffe610d1b7b66c9261ff276c98e
Pilla <manojkumar.pilla@dxc.com>
2023-05-05 09:17:56 +0530

    update

---
+++ repos/linq1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace linq1
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+
+    internal class Program
+    {
+        public static void Main()
+        {
+            List<Employee> listEmployees = new List<Employee>();
+            {
+                new Employee { Id = 1, Name = "Manoj" },
+                new Employee { Id = 2, Name = "Sai" },
+                new Employee { Id = 3, Name = "Manu" };
+
+            }
+
+            Employee employee = listEmployees.Find(x => x.Id == 2);
+            Console.WriteLine("Id = {0}, Name = {1}", employee.Id, employee.Name);
+
+           
+
+        }
+    }
+}
